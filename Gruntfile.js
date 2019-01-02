4a5e26267d55f6191df85015c3c10b55de12338c
unknown <jasmine.morvan@gmail.com>
2019-01-02 15:19:44 +0100

    update 2019

---
+++ Gruntfile.js
@@ -0,0 +1,233 @@
+module.exports = function(grunt) {
+
+  // /require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+
+    /* >> ------- Setup Diretories -------> */
+    dir: {
+      sources:'sources',
+      compiled:'assets',
+      imports:'imports',
+      images: 'img',
+      root: 'grunt-starter-sass'
+    },
+    /* << <-----  Setup Diretories ------- */
+
+
+    /* >> ------- Dependencies -------> */
+    autoprefixer: {
+      options: {
+         browsers: ['last 8 versions']
+      },
+      dist: { // Target
+         files: {
+           '<%= dir.compiled %>/css/main.css': '<%= dir.compiled %>/css/main.css'
+         }
+      },
+    },
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['<%= dir.sources %>/js/imports/*.js', '<%= dir.sources %>/js/main.js'],
+        dest: '<%= dir.compiled %>/js/main.js',
+      },
+    },
+    connect: {
+      server: {
+        options: {
+            port:8000,
+            hostname:'localhost',
+            base:'<%= dir.root %>/',
+            livereload:true
+        },
+      }
+    },
+    copy: {
+      images: {
+          expand: true,
+          cwd: '<%= dir.sources %>/<%= dir.images %>/',
+          src: '**',
+          dest: '<%= dir.compiled %>/<%= dir.images %>/',
+          //flatten: true,
+          filter: 'isFile',
+      }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: '/',
+          src: ['**/*.{jpg,gif}'],
+          dest: '<%= dir.compiled %>/<%= dir.images %>'
+        }]
+      }
+    },
+    jshint: {
+      files: ['<%= dir.sources %>/js/main.js'],
+      options: {
+         jquery: true,
+         smarttabs: true,
+         curly: true,
+         eqeqeq: true,
+         immed: true,
+         latedef: true,
+         newcap: true,
+         noarg: true,
+         sub: true,
+         undef: true,
+         boss: true,
+         eqnull: true,
+         browser: true
+      },
+      globals: {
+         jQuery: true,
+         console: true,
+         undef: true,
+         unused: false
+      }
+    },
+    pngmin: {
+      all: {
+        options: {
+          ext: '.png',
+          force: true
+        },
+        files: [
+          {
+            expand: true,
+            src: ['**/*.png'],
+            cwd: '<%= dir.sources %>/<%= dir.images %>',
+            dest: '<%= dir.compiled %>/<%= dir.images %>'
+          }
+        ]
+      },
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          '<%= dir.compiled %>/css/main.css': '<%= dir.sources %>/css/scss/main.scss',
+        }
+      }
+    },
+    stylus: {
+        compile: {
+            options: {
+
+            },
+            files: {
+              '<%= dir.compiled %>/css/main.css' : '<%= dir.sources %>/css/stylus/main.styl'
+            }
+        }
+    },
+    svgmin: {
+       options: {
+          plugins: [
+            {
+                removeViewBox: false
+            }, {
+                removeUselessStrokeAndFill: false
+            }
+          ]
+       },
+       dist: {
+          files: [
+             {
+               expand: true,
+               src: ['**/*.svg'],
+               cwd: '<%= dir.sources %>/<%= dir.images %>',
+               dest: '<%= dir.compiled %>/<%= dir.images %>'
+             }
+           ]
+        }
+     },
+
+
+    /* >> -----  Watcher ------- > */
+    watch: {
+      options: {
+        livereload: true,
+      },
+
+      concat: {
+        files: ['<%= dir.sources %>/js/**/*.js'],
+        tasks: ['concat']
+      },
+      data: {
+          files: ['data/**/*.csv'],
+          tasks: ['data'],
+      },
+      html: {
+        files: ['index.html'],
+      },
+      images: {
+         files: ['<%= dir.sources %>/<%= dir.images %>*.*'],
+         tasks: ['newer:imagemin:dynamic', 'newer:pngmin:all', 'newer:svgmin','newer:copy:images'],
+         options: {
+             spawn: false
+         }
+      },
+      jshint: {
+        files: ['<%= dir.sources %>/js/main.js'],
+        tasks: ['jshint']
+      },
+      sass: {
+        options: {
+         livereload: true,
+        },
+        files: ['<%= dir.sources %>/css/scss/**/*.scss'],
+         tasks: ['sass', 'autoprefixer'],
+      },
+      stylus:{
+        options: {
+          livereload: true,
+        },
+          files: ['<%= dir.sources %>/css/stylus/**/*.styl'],
+          tasks: ['stylus', 'autoprefixer'],
+      },
+    },
+    /* << <-----  Watcher ------- */
+
+
+  });
+
+  /* >> -----  Load Tasks -------> */
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.loadNpmTasks('grunt-contrib-sass'); // SCSS     CSS/SCSS
+  grunt.loadNpmTasks('grunt-contrib-stylus');      //  CSS/Stylus
+  grunt.loadNpmTasks('grunt-autoprefixer'); // AUTOPREFIX
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');  // DBUG JS
+  grunt.loadNpmTasks('grunt-contrib-concat');  // CONCAT
+
+  grunt.loadNpmTasks('grunt-newer');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-pngmin');
+  grunt.loadNpmTasks('grunt-svgmin');
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-notify');
+
+  /* << <-----  Load Tasks ------- */
+
+
+  /* >> -----  Run Tasks -------> */
+
+
+  grunt.registerTask('default', ['connect', 'watch']);
+  grunt.registerTask('imgmin', ['imagemin', 'pngmin:all', 'svgmin']);
+
+
+  /* << <-----  Load Tasks ------- */
+
+};
---
+++ sources/js/main.js
@@ -0,0 +1,146 @@
+$(function() {
+
+  // Tricks for IOs
+  $('body').on('touchstart', function(e) {
+    $('.container').css("pointer-events","auto");
+  });
+  $('body').on('touchmove', function(e) {
+      $('.container').css("pointer-events","none");
+  });
+  $('body').on('touchend', function(e) {
+      setTimeout(function() {
+          $('.container').css("pointer-events", "none");
+      },0);
+  });
+
+
+    var getCurentImageWidth = 0;
+
+    function resizeImgs(){
+        // Formatage des images au ratio
+        $('.image-container.bg').each(function(){
+            var src = $(this).data("src");
+
+            var ratioContainer = ($(this).parent().width()) / ($(this).parent().height());
+            if (navigator.userAgent.indexOf('Safari') !== -1 && navigator.userAgent.indexOf('Chrome') === -1 && navigator.userAgent.indexOf('604.1') === -1) {
+                $(this).attr("src", src);
+                $(this).addClass("safari");
+            }
+            if(!isFinite(ratioContainer) || ratioContainer === 0){
+                return;
+            }
+            // landscape ratio
+            if (ratioContainer >= 1.5){
+                src = "assets/img/" + src + "-l.png";
+                $(this).css("background-image", "url('"+src+"')");
+
+                $(this).attr("src", src);
+                $(this).addClass("landscape").removeClass("portrait");
+            }
+            // square landscape
+            else if (ratioContainer < 1.5 && ratioContainer >= 1) {
+                src = "assets/img/" + src + "-sq.png";
+                $(this).css("background-image", "url('"+src+"')");
+                $(this).addClass("landscape").removeClass("portrait");
+            }
+            // square portrait
+            else if (ratioContainer < 1 && ratioContainer >= 0.75) {
+                src = "assets/img/" + src + "-sq.png";
+                $(this).css("background-image", "url('"+src+"')");
+                $(this).addClass("portrait").removeClass("landscape").removeClass("safari");
+            }
+            // portrait
+            else if (ratioContainer < 0.75){
+                src = "assets/img/" + src + "-p.png";
+                $(this).css("background-image", "url('"+src+"')");
+                $(this).addClass("portrait").removeClass("landscape");
+            }
+        });
+    }
+
+
+
+
+
+    // Animation Decouvrir niv-0 gammes
+    $("article").each(function(){
+
+      $(this).click(function(){
+        if($(this).hasClass('main')){
+           return;
+       } else {
+         $("article").removeClass('main').addClass();
+         $(this).addClass('main');
+
+        $("article:not(.main)").addClass("shrink-x");
+
+         var getTarget = $(this).attr('data-target');
+         var getSrc = $(this).attr('id');
+
+         $("a.btn").css("display","none");
+
+         $(".quarter").addClass("half").removeClass("quarter");
+
+         $(this).children().last().addClass("shrink-y");
+
+         $("#pentax-nav").load(getTarget + ".html");
+         $("#pentax-nav").addClass( getSrc + " gamme");
+         $("#back").addClass("stretch-x");
+         $("#back").fadeIn();
+       }
+       //resizeImgs();
+     });
+    });
+
+
+
+    // Queries menu retour
+    function resizeBacknav(){
+        if ($(window).width() >= 978){
+            $("#back").removeClass("mobile");
+            $("#back").addClass("desktop");
+        } else {
+            $("#back").addClass("mobile");
+            $("#back").removeClass("desktop");
+        }
+    }
+
+    resizeBacknav();
+    // Ouverture menu retour
+    $(".btn").click(function(){
+        $("#back").addClass("stretch-x");
+        // Menu horizontal ou vertical
+        $("#back").fadeIn();
+
+    });
+
+
+    // Animation Retour
+        $("#back").click(function(){
+            $(this).removeClass("stretch-x");
+            $("a.btn").fadeTo(500,1);
+            $("article").removeClass("shrink-x");
+            $(".content").removeClass("shrink-y");
+            $("#pentax-nav").load("nav-0.html");
+
+            $("#pentax-nav").attr("class", "quarter");
+            $("article").removeClass("shrink-x");
+            $(".half").addClass("quarter").removeClass("half");
+            $("article").removeClass("main");
+            //resizeImgs();
+        });
+
+
+    $(window).resize(function() {
+        resizeImgs();
+        resizeBacknav();
+    });
+
+    $(document).ready(function() {
+        // Formatage des images au ratio - Affichage
+        resizeImgs();
+        $("#pentax").fadeTo(500,1);
+    });
+
+
+}); // ---------------- FONCTION GLOBALE  ----------------- //
